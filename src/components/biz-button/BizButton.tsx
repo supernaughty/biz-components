d3687b97eb1e470a30a0e3d37d3f237aaa309539
Code cat <supernaughty@naver.com>
2023-06-06 21:38:09 +0900

    Button modified & Typescript Version downgraded

--- src/components/biz-button/BizButton.tsx
+++ src/components/biz-button/BizButton.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import './BizButton.scss'
 
 interface BizButtonProps {
   children: React.ReactNode
 }
 
 const BizButton: React.FC<BizButtonProps> = ({ children }: BizButtonProps) => {
-  return <button>{children}</button>
+  return <button className="biz-button">{children}</button>
 }
 
 export default BizButton
